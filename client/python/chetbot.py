f7958f65a4872485db89928b1690cb8bd19ad5f0
Chetan Padia <chetan@swiftkey.com>
2015-06-04 12:32:55 +0100

    :minor refactor

--- client/python/chetbot.py
+++ client/python/chetbot.py
@@ -11,32 +11,36 @@ host = 'http://192.168.0.21:8897'
 
 class View:
 
-    def __init__(self, text, type=None, id=None):
+    def __init__(self, text=None, type=None, id=None):
         self.__commands = []
-        self.__commands.append({NAME: 'VIEW',
-                                ARGS: [text, type, id]})
+        if text or type or id:
+            self.__add('VIEW', text, type, id)
 
     def text(self):
-        self.__commands.append({NAME: 'TEXT'})
-        r = self.__execute()
+        self.__add('TEXT')
+        return self.__execute()
+
+    def tap(self):
+        self.__add('TAP')
+        self.__execute()
+        return self
+
+    def __add(self, cmd, *args):
+       self.__commands.append({NAME: cmd,
+                               ARGS: args})
+       return self
+
+    def __execute(self):
+        r = requests.post(host, params={'commands':json.dumps(self.__commands)})
         if r.status_code != 200:
             raise Exception(r.content)
         return json.loads(r.content)
-
-    def tap(self):
-        self.__commands.append({NAME: 'TAP'})
-        return self.__execute()
-
-    def __execute(self):
-        return requests.post(host, params={'commands':json.dumps(self.__commands)})
 
 __ = View
 wait = time.sleep
 
 if __name__ == '__main__':
-    from time import sleep
-
     __('start').tap()
     wait(1)
     __('stop').tap()
-
+    print __('mins').text()
