7ebe26f6fe7323cb1109c1e493832586e4720ed6
Chetan Padia <chetan@swiftkey.com>
2015-06-24 14:45:09 +0100

    tabbing

--- server/public/js/chetbot.js
+++ server/public/js/chetbot.js
@@ -8,69 +8,67 @@ var _ws = new ReconnectingWebSocket('ws://ec2-54-77-127-243.eu-west-1.compute.am
 /* Heartbeat to prevent device timing out */
 var device_iframe = document.querySelector('iframe');
 setInterval(function() { device_iframe.contentWindow.postMessage('heartbeat', '*'); },
-            60 * 1000);
+      60 * 1000);
 
 function __(text_or_options) {
 
-    var _commands = [];
+  var _commands = [];
 
-    function unhandled_data(e) {
-        console.error('Unhandled response', e.data);
+  function unhandled_data(e) {
+    console.error('Unhandled response', e.data);
+  }
+  _ws.onmessage = unhandled_data;
+
+  function _log_result(r) {
+    console.log(r);
+  }
+
+  function _add(cmd, args) {
+    _commands.push({name: cmd,
+            args: args || []});
+  }
+
+  function _execute(result_handler) {
+    result_handler = result_handler || _log_result;
+    var msg = {
+      'request':  'UUID_GOES_HERE', // TODO
+      'device':   chetbot_device,
+      'commands': _commands
     }
-    _ws.onmessage = unhandled_data;
-
-    function _log_result(r) {
-        console.log(r);
+    _ws.send(JSON.stringify(msg));
+    _ws.onmessage = function(e) {
+      _ws.onmessage = unhandled_data;
+      var resp = JSON.parse(e.data);
+      if (resp.error) {
+        console.error(resp.error);
+      } else {
+        result_handler(resp.result);
+      }
     }
+  }
 
-    function _add(cmd, args) {
-        _commands.push({name: cmd,
-                        args: args || []});
+  function view() {}
+
+  var selectors_cmds = ['view', 'leftmost', 'rightmost', 'topmost', 'bottommost', 'closest_to', 'further_from'];
+  selectors_cmds.forEach(function(cmd) {
+    view[cmd] = function() {
+      _add(cmd.toUpperCase(), [].slice.call(arguments));
+      return view;
     }
+  });
 
-    function _execute(result_handler) {
-        if (result_handler === undefined) {
-            result_handler = _log_result;
-        }
-        var msg = {
-            'request':  'UUID_GOES_HERE', // TODO
-            'device':   chetbot_device,
-            'commands': _commands
-        }
-        _ws.send(JSON.stringify(msg));
-        _ws.onmessage = function(e) {
-            _ws.onmessage = unhandled_data;
-            var resp = JSON.parse(e.data);
-            if (resp.error) {
-                console.error(resp.error);
-            } else {
-                result_handler(resp.result);
-            }
-        }
+  var action_cmds = ['tap', 'text', 'id', 'type', 'count', 'exists', 'location', 'center', 'size'];
+  action_cmds.forEach(function(cmd) {
+    view[cmd] = function() {
+      _add(cmd.toUpperCase(), [].slice.call(arguments, 1));
+      _execute(arguments[0]);
+      return view;
     }
+  });
 
-    function view() {}
+  view.view(text_or_options);
 
-    var selectors_cmds = ['view', 'leftmost', 'rightmost', 'topmost', 'bottommost', 'closest_to', 'further_from'];
-    selectors_cmds.forEach(function(cmd) {
-        view[cmd] = function() {
-            _add(cmd.toUpperCase(), [].slice.call(arguments));
-            return view;
-        }
-    });
-
-    var action_cmds = ['tap', 'text', 'id', 'type', 'count', 'exists', 'location', 'center', 'size'];
-    action_cmds.forEach(function(cmd) {
-        view[cmd] = function() {
-            _add(cmd.toUpperCase(), [].slice.call(arguments, 1));
-            _execute(arguments[0]);
-            return view;
-        }
-    });
-
-    view.view(text_or_options);
-
-    return view;
+  return view;
 }
 
 window.__ = __;
