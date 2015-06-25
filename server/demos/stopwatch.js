d934a61a81502bb96106ae981f11eb876fa9eac4
Chetan Padia <chetan@swiftkey.com>
2015-06-25 10:39:09 +0100

    add wait(...) to demo

--- server/demos/stopwatch.js
+++ server/demos/stopwatch.js
@@ -6,5 +6,12 @@
 // Gets the ID of the text view denoting seconds
 __('00').closest_to('secs').id();
 
+// Reset the timer
+__('reset').tap();
+
 // Tap the start button
 __('start').tap();
+
+// Stop after 3 seconds
+wait(3);
+__('stop').tap();
