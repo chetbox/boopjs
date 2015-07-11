60aa5d584b599a7fdd45dce66bc17f47c632103a
Chetan Padia <chetan@swiftkey.com>
2015-07-11 13:29:19 +0100

    find the smali file that needs modifying

--- scripts/make-chetbot-apk.sh
+++ scripts/make-chetbot-apk.sh
@@ -8,7 +8,6 @@ fi
 # fail on error
 set -e
 
-cwd="$PWD"
 file=$(basename "$1")
 extension="${file##*.}"
 filename="${file%.*}"
@@ -18,39 +17,47 @@ tmp_dir=/tmp/make_chetbot_apk
 rm -rf "$tmp_dir" # make sure there's no junk lying around
 mkdir -p "$tmp_dir"
 
-outfile_unaligned="${tmp_dir}/${filename}.unaligned.chetbot.${extension}"
-
 echo "Copying APK"
+echo "  $1"
 cp "$1" "${tmp_dir}/app.apk"
 
 echo "Copying ChetBot sources"
 cp -r chetbot-smali "${tmp_dir}/"
 
-cd "${tmp_dir}"
-
 echo "Extracting classes.dex"
-unzip -p app.apk classes.dex > classes.dex
+unzip -p "${tmp_dir}/app.apk" classes.dex > "${tmp_dir}/classes.dex"
 
 echo "Decompiling"
-baksmali -x classes.dex -o app-smali
+baksmali -x "${tmp_dir}/classes.dex" -o "${tmp_dir}/app-smali" > /dev/null
+
+echo "Finding main activity"
+main_activity=$(java -jar APKParser.jar "${tmp_dir}/app.apk" | \
+                xml sel -t -v "/manifest/application/activity[intent-filter/category/@android:name='android.intent.category.LAUNCHER' and intent-filter/action/@android:name='android.intent.action.MAIN']/@android:name")
+echo "  $main_activity"
+
+main_activity_smali="${tmp_dir}/app-smali/$(echo $main_activity | sed 's#\.#/#g').smali"
+if [ ! -f "$main_activity_smali" ] ; then
+  >&2 echo "Error: Could not find code for main activity (${main_activity_smali})"
+  exit 1
+fi
 
 echo "Injecting Chetbot"
-cp -R chetbot-smali/* app-smali/
+cp -R chetbot-smali/* "${tmp_dir}/app-smali/"
 
 echo "TODO: launch chetbot on LAUNCHER activity"
-echo "Please add the following code manually in $tmp_dir/app-smali and press Enter"
+echo "Please add the following code manually to ${main_activity_smali} and press Enter"
 echo "    invoke-static {p0}, Lcom/chetbox/chetbot/android/Chetbot;->start(Landroid/app/Activity;)V"
 read
 
 echo "Recompiling"
-rm classes.dex
-smali app-smali -o classes.dex
+rm "${tmp_dir}/classes.dex"
+smali "${tmp_dir}/app-smali" -o "${tmp_dir}/classes.dex" > /dev/null
 
-echo "Modifying APK"
-zip app.apk classes.dex
+echo "Updating APK"
+zip -j "${tmp_dir}/app.apk" "${tmp_dir}/classes.dex"
 
 echo "Removing old signing info"
-zip -d app.apk "META-INF/*"
+zip -d "${tmp_dir}/app.apk" "META-INF/*"
 
 echo "Signing"
 jarsigner \
@@ -59,16 +66,15 @@ jarsigner \
   -keystore ~/.android/debug.keystore \
   -storepass android \
   -keypass android \
-  app.apk \
+  "${tmp_dir}/app.apk" \
   androiddebugkey
-jarsigner -verify -certs app.apk
+jarsigner -verify -certs "${tmp_dir}/app.apk"
 
 echo "Zip-aligning"
-zipalign 4 app.apk app-aligned.apk
+zipalign 4 "${tmp_dir}/app.apk" "${tmp_dir}/app-aligned.apk"
 
 echo "Cleaning up"
-cp app-aligned.apk "${cwd}/${outfilename}"
-cd "${cwd}"
+cp "${tmp_dir}/app-aligned.apk" "${outfilename}"
 rm -r "${tmp_dir}"
 
 echo "Created ${outfilename}"
