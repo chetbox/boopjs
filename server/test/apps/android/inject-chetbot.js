b90cf8995e21f84f4aa7ff86da1b620c166d2278
Chetan Padia <chetan@swiftkey.com>
2015-08-07 00:16:54 +0100

    allow for AndroidManifest.xml files with <activity-alias> main activities

--- server/test/apps/android/inject-chetbot.js
+++ server/test/apps/android/inject-chetbot.js
@@ -6,14 +6,22 @@ describe('Inject Chetbot into Android APK', function() {
 
   var inject = require('../../../apps/android/inject-chetbot');
 
-  var apk = path.join(__dirname, 'fixtures', 'stopwatch-debug.apk');
+  var stopwatch_apk = path.join(__dirname, 'fixtures', 'stopwatch-debug.apk');
+  var mubi_apk = path.join(__dirname, 'fixtures', 'com.mubi.apk');
   var myapp_smali = fs.readFileSync(path.join(__dirname, 'fixtures', 'myapp-smali', 'myapp', 'MyMainActivity.smali'));
   var myapp_smali_with_chetbot = String(fs.readFileSync(path.join(__dirname, 'fixtures', 'myapp-chetbot-smali', 'myapp', 'MyMainActivity.smali')));
 
-  it('finds the launcher activity', function() {
+  it('finds the launcher <activity>', function() {
     assert.equal(
-      inject.find_main_activity(apk),
+      inject.find_main_activity(stopwatch_apk),
       'com.chetbox.chetbot.stopwatch.Stopwatch'
+    );
+  });
+
+  it('finds the launcher <activity-alias>', function() {
+    assert.equal(
+      inject.find_main_activity(mubi_apk),
+      'com.mubi.browse.BrowseActivity'
     );
   });
 
