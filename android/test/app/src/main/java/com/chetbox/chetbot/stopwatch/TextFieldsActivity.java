6e47be61c87fb16fc19ae2fd03dbfa072084b645
Chetan Padia <chetan@swiftkey.com>
2015-08-06 12:02:12 +0100

    created TextFields activity

---
+++ android/test/app/src/main/java/com/chetbox/chetbot/stopwatch/TextFieldsActivity.java
@@ -0,0 +1,14 @@
+package com.chetbox.chetbot.stopwatch;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TextFieldsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.text_fields);
+    }
+}
