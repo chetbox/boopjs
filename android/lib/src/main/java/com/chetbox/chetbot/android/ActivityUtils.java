dc7f8e9dfc296db7fe03fcae2f4f2e39e15ea8fd
Chetan Padia <chetan@swiftkey.com>
2015-08-16 11:47:43 +0100

    wait until idle after interactions

---
+++ android/lib/src/main/java/com/chetbox/chetbot/android/ActivityUtils.java
@@ -0,0 +1,75 @@
+package com.chetbox.chetbot.android;
+
+import android.app.Activity;
+import android.os.Looper;
+import android.os.MessageQueue;
+import android.util.ArrayMap;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.CountDownLatch;
+
+public class ActivityUtils {
+
+    private static final String TAG = ActivityUtils.class.getSimpleName();
+
+    public static Activity getActivity(String packageName) {
+        // based on https://androidreclib.wordpress.com/2014/11/22/getting-the-current-activity/
+        try {
+            Class activityThreadClass = Class.forName("android.app.ActivityThread");
+            Object activityThread = activityThreadClass.getMethod("currentActivityThread").invoke(null);
+            Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
+            activitiesField.setAccessible(true);
+            // TODO: handle API < 19
+            // (ArrayMap is new for API 19)
+            ArrayMap activities = (ArrayMap) activitiesField.get(activityThread);
+            for (Object activityRecord : activities.values()) {
+                Class activityRecordClass = activityRecord.getClass();
+                Field pausedField = activityRecordClass.getDeclaredField("paused");
+                pausedField.setAccessible(true);
+                if (!pausedField.getBoolean(activityRecord)) {
+                    Field activityField = activityRecordClass.getDeclaredField("activity");
+                    activityField.setAccessible(true);
+                    Activity activity = (Activity) activityField.get(activityRecord);
+                    if (activity.getPackageName().equals(packageName)) {
+                        return activity;
+                    } else {
+                        Log.v(TAG, "Found activity for different package: " + activity.getPackageName());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        throw new AssertionError("Activity not found");
+    }
+
+    public static void sleep(Double seconds) {
+        try {
+            Thread.sleep(Math.round(seconds * 1000.0));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void waitUntilIdle(final Activity activity) {
+        final CountDownLatch idleLatch = new CountDownLatch(1);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.myQueue().addIdleHandler(new MessageQueue.IdleHandler() {
+                    @Override
+                    public boolean queueIdle() {
+                        idleLatch.countDown();
+                        return false; // Do not repeat
+                    }
+                });
+            }
+        });
+        try {
+            idleLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
