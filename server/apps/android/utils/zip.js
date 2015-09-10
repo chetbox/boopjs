45daed63d39924d0991ca114515c19cfef48d73d
Chetan Padia <chetbox@gmail.com>
2015-09-10 22:49:35 +0100

    hide internals of Zip module in zip utils

--- server/apps/android/utils/zip.js
+++ server/apps/android/utils/zip.js
@@ -1,17 +1,21 @@
 var Zip = require('adm-zip');
 
 exports.extract = function(zip_file, match_fn) {
-  return new Zip(zip_file)
+  var files = {};
+  new Zip(zip_file)
   .getEntries()
   .filter(function(entry) {
     return match_fn(entry.entryName);
+  })
+  .forEach(function(entry) {
+    files[entry.entryName] = entry.getData();
   });
+  return files;
 }
 
 exports.extract_file = function(zip_file, file_name) {
   return exports.extract(zip_file, function(f) {
     return f === file_name;
   })
-  [0]
-  .getData();
+  [file_name];
 };
--- server/test/apps/android/utils/zip.js
+++ server/test/apps/android/utils/zip.js
@@ -10,17 +10,15 @@ describe('Zip utils', function() {
 
   it('"extract" extracts files matching a filter', function() {
     var launcher_icons = zip_utils.extract(apk, function(file_path) {
-      return file_path.match(/ic_launcher\.png/);
-    })
-    .map(function(entry) {
-      return entry.entryName;
+      return file_path.match(/res\/mipmap-mdpi.*\/ic_launcher\.png/);
     });
-    assert.deepEqual(launcher_icons, [
-      'res/mipmap-hdpi-v4/ic_launcher.png',
-      'res/mipmap-mdpi-v4/ic_launcher.png',
-      'res/mipmap-xhdpi-v4/ic_launcher.png',
-      'res/mipmap-xxhdpi-v4/ic_launcher.png'
-    ]);
+    // convert to base64 for comparison
+    Object.keys(launcher_icons).forEach(function(file_path) {
+      launcher_icons[file_path] = launcher_icons[file_path].toString('base64');
+    });
+    assert.deepEqual(launcher_icons, {
+      'res/mipmap-mdpi-v4/ic_launcher.png': mdpi_icon.toString('base64')
+    });
   });
 
   it('"extract_file" extracts the contents of a file in the archive', function() {
