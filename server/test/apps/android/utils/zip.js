bddcb7fb9f36a6392ea1f20835e8e88ae5bb1d0c
Chetan Padia <chetbox@gmail.com>
2015-09-10 22:28:03 +0100

    added zip extract with filter function

--- server/test/apps/android/utils/zip.js
+++ server/test/apps/android/utils/zip.js
@@ -8,7 +8,17 @@ describe('Zip utils', function() {
   var apk = path.join(__dirname, '..', 'fixtures', 'stopwatch-debug.apk');
   var mdpi_icon = fs.readFileSync(path.join(__dirname, 'fixtures', 'ic_launcher-mdpi.png'));
 
-  it('extract_file extracts the contents of a file in the archive', function() {
+  it('"extract" extracts files matching a filter', function() {
+    var launcher_icons = zip_utils.extract(apk, function(file_path) {
+      return file_path.match(/res\/mipmap-mdpi.*\/ic_launcher\.png/);
+    })
+    .map(function(data) {
+      return data.toString('base64');
+    });
+    assert.deepEqual(launcher_icons, [mdpi_icon.toString('base64')]);
+  });
+
+  it('"extract_file" extracts the contents of a file in the archive', function() {
     var extracted_icon = zip_utils.extract_file(apk, 'res/mipmap-mdpi-v4/ic_launcher.png');
     assert.equal(extracted_icon.toString('base64'), mdpi_icon.toString('base64'));
   });
