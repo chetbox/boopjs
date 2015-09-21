6528866d4ee7d3f29f252a0320d2e0b1d15c9b6e
Chetan Padia <chetbox@gmail.com>
2015-09-21 23:43:02 +0100

    minor: refactor

--- server/public/js/repl.js
+++ server/public/js/repl.js
@@ -9,53 +9,56 @@ function setup_repl(server, device_id, id, callbacks) {
     enableSnippets: false
   });
 
-  $repl.keydown(function(e) {
-    if (e.which == 27 /* Esc */) {
-      if (callbacks && callbacks.onClose) callbacks.onClose();
+  function run() {
+    if (repl.getReadOnly()) {
+      // Still running
       return;
     }
 
-    if (e.which == 13 /* Return */) {
-      e.preventDefault();
-      if (repl.getReadOnly()) {
-        // Still running
+    repl.setReadOnly(true);
+    $repl.addClass('running');
+
+    var doc = repl.getSession().getDocument();
+    var src_to_execute = doc.getLine(doc.getLength() - 1);
+    doc.insertLines(doc.getLength(), ['']); // Used for result or next command
+    repl.gotoLine(doc.getLength());
+
+    function show_result(r) {
+      if (!r.error && (r.result === undefined || r.result === null)) {
         return;
       }
 
-      repl.setReadOnly(true);
-      $repl.addClass('running');
-
-      var doc = repl.getSession().getDocument();
-      var src_to_execute = doc.getLine(doc.getLength() - 1);
-      doc.insertLines(doc.getLength(), ['']); // Used for result or next command
-      repl.gotoLine(doc.getLength());
-
-      function show_result(r) {
-        if (!r.error && (r.result === undefined || r.result === null)) {
-          return;
-        }
-
-        repl.getSession().addGutterDecoration(doc.getLength() - 1, 'result');
-        if (r.error) {
-          repl.getSession().addGutterDecoration(doc.getLength() - 1, 'error');
-        }
-
-        var message = '// ' + JSON.stringify(r.error ? r.error : r.result);
-        doc.insertLines(doc.getLength() - 1, [message]);
-        repl.gotoLine(doc.getLength());
+      repl.getSession().addGutterDecoration(doc.getLength() - 1, 'result');
+      if (r.error) {
+        repl.getSession().addGutterDecoration(doc.getLength() - 1, 'error');
       }
 
-      // execute code
-      run_script(server, device_id, [{line: 1, source: src_to_execute}], {
-        onResult: show_result,
-        onError: show_result
-      });
+      var message = '// ' + JSON.stringify(r.error ? r.error : r.result);
+      doc.insertLines(doc.getLength() - 1, [message]);
+      repl.gotoLine(doc.getLength());
+    }
 
-      repl.setReadOnly(true);
-      setTimeout(function() {
-        repl.setReadOnly(false);
-        $repl.removeClass('running');
-      }, 2000);
+    // execute code
+    run_script(server, device_id, [{line: 1, source: src_to_execute}], {
+      onResult: show_result,
+      onError: show_result
+    });
+
+    repl.setReadOnly(true);
+    setTimeout(function() {
+      repl.setReadOnly(false);
+      $repl.removeClass('running');
+    }, 2000);
+  }
+
+  $repl.keydown(function(e) {
+    switch (e.which) {
+      case 27: // Esc
+        if (callbacks && callbacks.onClose) callbacks.onClose();
+        return false;
+      case 13: // Return
+        run();
+        return false;
     }
   });
 
