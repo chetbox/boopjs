ccb2849aebd17bfcc909980cda13432397501451
Chetan Padia <chetbox@gmail.com>
2015-09-22 12:15:41 +0100

    working REPL history

--- server/public/js/repl.js
+++ server/public/js/repl.js
@@ -8,13 +8,38 @@ function setup_repl(server, device_id, id, callbacks) {
     enableLiveAutocompletion: true,
     enableSnippets: false
   });
+  repl.$blockScrolling = Infinity
+
+  var history = [];
+  var history_cursor = history.length;
+
+
+  function show_from_history(new_cursor_value) {
+    if (new_cursor_value < 0) return;
+    if (new_cursor_value > history.length) return;
+    history_cursor = new_cursor_value;
+    var doc = repl.getSession().getDocument();
+    var line_to_update = doc.getLength() - 1;
+    var new_line = history[history_cursor];
+    if (line_to_update === 0) {
+      doc.setValue(new_line);
+    } else {
+      doc.removeLines(line_to_update, line_to_update);
+      doc.insertLines(line_to_update, [new_line]);
+    }
+    repl.gotoLine(line_to_update + 1);
+  }
+
+  function add_to_history(line) {
+    if (history.length > 0 && history[history.length - 1] === line) {
+      // Same as the last line, do not add
+    } else {
+      history.push(line);
+    }
+    history_cursor = history.length;
+  }
 
   function run() {
-    if (repl.getReadOnly()) {
-      // Still running
-      return;
-    }
-
     repl.setReadOnly(true);
     $repl.addClass('running');
 
@@ -38,6 +63,8 @@ function setup_repl(server, device_id, id, callbacks) {
       repl.gotoLine(doc.getLength());
     }
 
+    add_to_history(src_to_execute);
+
     // execute code
     run_script(server, device_id, [{line: 1, source: src_to_execute}], {
       onResult: show_result,
@@ -51,16 +78,45 @@ function setup_repl(server, device_id, id, callbacks) {
     }, 2000);
   }
 
-  $repl.keydown(function(e) {
-    switch (e.which) {
-      case 27: // Esc
-        if (callbacks && callbacks.onClose) callbacks.onClose();
-        return false;
-      case 13: // Return
-        run();
-        return false;
+  repl.commands.addCommands([
+    {
+      name: 'Close',
+      bindKey: 'Esc',
+      exec: callbacks && callbacks.onClose
+    }, {
+      name: 'Run',
+      bindKey: 'Return',
+      exec: function() {
+        if (!repl.getReadOnly()) {
+          run();
+        }
+      }
+    }, {
+      name: 'Previous command',
+      bindKey: 'Up',
+      exec: function() {
+        if (!repl.getReadOnly()) {
+          show_from_history(history_cursor - 1);
+        }
+      }
+    }, {
+      name: 'Next command',
+      bindKey: 'Down',
+      exec: function() {
+        if (!repl.getReadOnly()) {
+          show_from_history(history_cursor + 1);
+        }
+      }
+    }, {
+      name: 'Page up (disabled)',
+      bindKey: 'PageUp',
+      exec: function() {}
+    }, {
+      name: 'Page down (disabled)',
+      bindKey: 'PageDown',
+      exec: function() {}
     }
-  });
+  ]);
 
   return repl;
 }
