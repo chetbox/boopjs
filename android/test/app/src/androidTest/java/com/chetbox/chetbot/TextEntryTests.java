39d0eef1176e97f71e4d65803ee24b42fe59a19d
Chetan Padia <chetbox@gmail.com>
2015-10-11 16:35:07 +0100

    fix type_text

--- android/test/app/src/androidTest/java/com/chetbox/chetbot/TextEntryTests.java
+++ android/test/app/src/androidTest/java/com/chetbox/chetbot/TextEntryTests.java
@@ -10,16 +10,16 @@ import static org.hamcrest.Matchers.equalTo;
 public class TextEntryTests extends TextFieldsTest {
 
     @Test public void typeInTextField() {
-        exec("tap('text')");
-        exec("type_text('Once upon a time...')");
+        exec(   "tap('text');",
+                "type_text('Once upon a time...');");
 
         assertThat(textField.getText().toString(),
                 equalTo("Once upon a time..."));
     }
 
     @Test public void typeInPasswordField() {
-        exec("tap('password')");
-        exec("type_text('my!secret*passw0rd')");
+        exec(   "tap('password');",
+                "type_text('my!secret*passw0rd');");
 
         assertThat(passwordField.getText().toString(),
                 equalTo("my!secret*passw0rd"));
