b37546af60de2c12738e18e29d5c17aeb1eeee39
Chetan Padia <chetbox@gmail.com>
2015-12-07 11:08:40 +0000

    added function to start test runner

--- server/test_runner.js
+++ server/test_runner.js
@@ -1,10 +1,12 @@
+var config = require('config');
 var url = require('url');
 var shortid = require('shortid');
+var request = Promise.promisifyAll(require('request'));
 
 var db = require('./db');
 var fail_on_error = require('./util').fail_on_error;
 
-exports.create_access_token = function(endpoint) {
+function create_access_token(endpoint) {
   var token = shortid.generate();
   return db.run_tokens().insert({
     endpoint: endpoint,
@@ -17,7 +19,7 @@ exports.create_access_token = function(endpoint) {
 
 exports.consume_access_token = function(access_token_query_key) {
   return function(req, res, next) {
-    var endpoint = url.parse(req.url).pathname;
+    var endpoint = url.parse(req.originalUrl).pathname;
     var token = req.query[access_token_query_key];
     db.run_tokens().find({hash: endpoint, range: token})
     .then(function(run_token) {
@@ -32,4 +34,24 @@ exports.consume_access_token = function(access_token_query_key) {
     })
     .catch(fail_on_error(res));
   };
+}
+
+exports.run = function(app_id, code_id) {
+  var endpoint = '/app/' + app_id + '/run/' + code_id;
+  return create_access_token(endpoint)
+  .then(function(access_token) {
+    return request.postAsync({
+    url: config.test_runner.protocol + '://' + config.test_runner.host + '/run',
+    json: true,
+    body: {
+      url: config.host + '://' + endpoint + '?access_token=' + access_token,
+      script: '// TODO: send test results back to client'
+    }
+  })
+  .spread(function(resp, body) {
+    if (resp.statusCode !== 200) {
+      throw 'HTTP error ' + resp.statusCode + ': ' + body;
+    }
+    return body;
+  });
 }
