a8444ff029d0a527ad9900fb46b1c8fda0843533
Chetan Padia <chetbox@gmail.com>
2015-12-08 23:04:34 +0000

    Revert "shuffling things around in server.js"
    This reverts commit e987e5649e2a881d24f814f1c9bb98e857855aa1.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 require('shortid').seed(56873);
 
 var express = require('express');
+var app = express();
 var express_handlebars = require('express-handlebars');
 var moment = require('moment');
 var body_parser = require('body-parser');
@@ -11,18 +12,12 @@ var editor_demo = require('./editor_demo');
 var editor = require('./editor');
 
 
-// Express app
+// Settings
 
-var app = express();
-app.use(express.static(__dirname + '/public'));
-require('./auth')             .setup(app, {logged_in_homepage: '/apps'});
-require('./index')            .add_routes(app);
-require('./editor_demo')      .add_routes(app);
-require('./editor')           .add_routes(app);
-require('./websocket_server') .add_routes(app);
-require('./admin')            .add_routes(app);
+var port = process.env.PORT || 8001;
 
-// Mustache
+
+// Mustache setup
 
 var hbs = express_handlebars.create({
     defaultLayout: false,
@@ -50,9 +45,18 @@ app.use(body_parser.text());
 app.use(body_parser.urlencoded());
 
 
-// Launch
+// Application setup
 
-var port = process.env.PORT || 8001;
+app.use(express.static(__dirname + '/public'));
+require('./auth')             .setup(app, {logged_in_homepage: '/apps'});
+require('./index')            .add_routes(app);
+require('./editor_demo')      .add_routes(app);
+require('./editor')           .add_routes(app);
+require('./websocket_server') .add_routes(app);
+require('./admin')            .add_routes(app);
+
+
+// Launch
 
 app.listen(port, function() {
     console.log((new Date()) + ' Server is listening on port ' + port + ' with configuration: ' + process.env.NODE_ENV);
