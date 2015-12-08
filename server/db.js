f5cb7da64faab095ed29b9405f1db222e6553d5c
Chetan Padia <chetbox@gmail.com>
2015-12-08 22:06:35 +0000

    added new DB API without using Dynasty

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 var config = require('config');
-var dynasty = require('dynasty')(config.get('dynamodb'));
+var dynasty = require('dynasty')(config.get('aws.dynamodb'));
 var _ = require('underscore');
 
 var TABLE_PREFIX = 'chetbot.';
@@ -19,6 +19,14 @@ var TABLES = {
   'devices': {
     key_schema: {hash: ['id', 'string']},
     throughput: {read: 1, write: 1}
+  },
+  'run_tokens': {
+    key_schema: {hash: ['endpoint', 'string'], range: ['token', 'string']},
+    throughput: {read: 1, write: 1}
+  },
+  'results': {
+    key_schema: {hash: ['code_id', 'string'], range: ['started_at', 'number']},
+    throughput: {write: 1, read: 1}
   }
 };
 
@@ -42,4 +50,30 @@ _.each(TABLES, function(_, name) {
   exports[name] = function() {
     return dynasty.table(TABLE_PREFIX + name);
   };
+});
+
+// In progress: Migration away from Dyanasty
+
+var AWS = require('aws-sdk');
+AWS.config.update(config.get('aws.dynamodb'));
+var dynamodb = new AWS.DynamoDB.DocumentClient({
+  // service: {
+    // endpoint: config.dynamodb.endpoint
+  // }
+});
+require('bluebird').promisifyAll(Object.getPrototypeOf(dynamodb));
+
+// Provide a neat, promisified API with table already set
+// e.g. new_api.devices.put({...})
+exports.v2 = _.map(TABLES, function(_, name) {
+  var table = TABLE_PREFIX + name;
+  var fns = {};
+  ['update', 'put', 'get', 'scan', 'query', 'delete'].forEach(function(fn_name) {
+    fns[fn_name] = function(params) {
+      return dynamodb[fn_name + 'Async'](_.merge(
+        params,
+        { TableName: table }
+      ));
+    };
+  });
 });
