9ca214e6dbe976f2d08f292cf2803b2d2287415c
Chetan Padia <chetbox@gmail.com>
2015-12-10 00:47:43 +0000

    fix some things

--- server/public/js/script-runner.js
+++ server/public/js/script-runner.js
@@ -1,4 +1,4 @@
-function run_script(server, device_id, code_id, statements, callbacks) {
+function run_script(server, device_id, app_id, code_id, statements, callbacks) {
 
   function callback(event, data) {
     $(document).trigger('test-progress', [event, data]);
@@ -15,6 +15,7 @@ function run_script(server, device_id, code_id, statements, callbacks) {
   var ws = new WebSocket(
     'ws://' + server + '/api/client'
     + '?device=' + encodeURIComponent(device_id)
+    + '&app=' + encodeURIComponent(app_id)
     + (code_id
         ? '&code=' + encodeURIComponent(code_id)
         : ''
@@ -34,7 +35,6 @@ function run_script(server, device_id, code_id, statements, callbacks) {
     if ('error' in message) {
       callback('onError', message);
       callback('onFinish', false);
-      ws.close();
     } else if ('result' in message) {
       callback('onResult', message);
     } else if ('log' in message) {
@@ -42,7 +42,6 @@ function run_script(server, device_id, code_id, statements, callbacks) {
     } else if ('success' in message) {
       callback('onSuccess', message);
       callback('onFinish', true);
-      ws.close();
     }
   };
 }
