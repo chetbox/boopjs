3ecdf6cc8c3f4394bcbb7e4d2af89b340aaa7850
Chetan Padia <chetbox@gmail.com>
2016-01-10 22:11:48 +0000

    added model for code

---
+++ server/model/code.js
@@ -0,0 +1,40 @@
+var shortid = require('shortid');
+var fs = require('fs');
+var _ = require('underscore');
+
+var db = require('../db').v2.code;
+var debug = require('debug')('chetbot/' + require('path').relative(process.cwd(), __filename).replace(/\.js$/, ''));
+
+var welcome_code = fs.readFileSync(__dirname + '/../demos/welcome.js', 'utf8');
+
+exports.get = function(app_id, code_id) {
+  debug('get', app_id, code_id);
+  return db.get({Key: {app_id: app_id, id: code_id}});
+};
+
+exports.create = function(app_id) {
+  debug('create', app_id);
+  var item = {
+    id: shortid.generate(),
+    name: 'Untitled test',
+    app_id: app_id,
+    content: welcome_code
+  };
+  return db.put({Item: item})
+  .then(function() {
+    return item;
+  });
+};
+
+exports.set_latest_result = function(result) {
+  debug('set_latest_result', result.code_id, result.started_at);
+  return db.update({
+    Key: {app_id: result.app.id, id: result.code_id},
+    UpdateExpression: 'SET latest_result = :result',
+    ConditionExpression: 'attribute_not_exists(latest_result) OR latest_result.started_at <= :started_at',
+    ExpressionAttributeValues: {
+      ':result': _.pick(result, ['started_at', 'success', 'error']),
+      ':started_at': result.started_at
+    }
+  });
+};
