395c6457c32d01baefe4eeafa3903a3a26c52d24
Chetan Padia <chetbox@gmail.com>
2016-01-25 22:39:04 +0000

    added pricing page

---
+++ server/routes/public.js
@@ -0,0 +1,39 @@
+exports.add_routes = function(app) {
+
+  app.get('/', function(req, res) {
+    res.render('landing-page');
+  });
+
+  app.get('/pricing', function(req, res) {
+    res.render('pricing', {
+      tiers: [
+        {
+          name: 'Free',
+          monthly_cost: '$0/mo',
+          app_limit: 1,
+          test_limit: 5,
+          current: true
+        },
+        {
+          name: 'Indie',
+          monthly_cost: '$200/mo',
+          app_limit: 2,
+          test_limit: null
+        },
+        {
+          name: 'Team',
+          monthly_cost: '$500/mo',
+          app_limit: 10,
+          test_limit: null
+        },
+        {
+          name: 'Unlimited',
+          monthly_cost: 'Contact us',
+          app_limit: null,
+          test_limit: null
+        }
+      ]
+    });
+  });
+
+};
