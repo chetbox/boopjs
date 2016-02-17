d2ec23c4900ea22f1823268db42977fd09f1daa3
Chetan Padia <chetbox@users.noreply.github.com>
2016-02-17 16:46:08 +0000

    show "dashboard" instead of "sign in" on lnading page if signed in

--- server/routes/public.js
+++ server/routes/public.js
@@ -1,7 +1,9 @@
 exports.add_routes = function(app) {
 
   app.get('/', function(req, res) {
-    res.render('landing-page');
+    res.render('landing-page', {
+      user: req.user
+    });
   });
 
   app.get('/docs', function(req, res) {
