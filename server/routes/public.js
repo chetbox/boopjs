92744172610e155ee0e500f7f0c49ade1b22084d
Chetan Padia <chetbox@users.noreply.github.com>
2016-02-17 13:39:26 +0000

    cards on pricing page, factor out sign in button

--- server/routes/public.js
+++ server/routes/public.js
@@ -10,31 +10,35 @@ exports.add_routes = function(app) {
 
   app.get('/pricing', function(req, res) {
     res.render('pricing', {
+      user: req.user,
       tiers: [
         {
           name: 'Free',
           monthly_cost: '$0/mo',
           app_limit: 1,
           test_limit: 5,
-          current: true
+          current: !!req.user
         },
         {
           name: 'Indie',
           monthly_cost: '$200/mo',
           app_limit: 2,
-          test_limit: null
+          test_limit: null,
+          support: 'Speedy email support'
         },
         {
           name: 'Team',
           monthly_cost: '$500/mo',
           app_limit: 10,
-          test_limit: null
+          test_limit: null,
+          support: 'Telephone support'
         },
         {
           name: 'Unlimited',
           monthly_cost: 'Contact us',
           app_limit: null,
-          test_limit: null
+          test_limit: null,
+          support: 'Telephone support'
         }
       ]
     });
